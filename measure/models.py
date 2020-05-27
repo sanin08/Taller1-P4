f7a57762ff8134e4dc71e96043e18635f8aa5b6c
jssaninv <jssaninv@eafit.edu.co>
2020-05-27 18:34:07 -0500

    despliegu43

--- measure/models.py
+++ measure/models.py
@@ -5,5 +5,7 @@ class Measure(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(verbose_name='Tipo', max_length=20)
     value = models.IntegerField(verbose_name='Valor')
+    latitud = models.IntegerField(verbose_name='Latitud')
+    longitud = models.IntegerField(verbose_name='Longitud')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
